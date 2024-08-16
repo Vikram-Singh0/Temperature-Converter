f99de29477a591f26e6c5b8bcc56aac780456b42
Vikram-Singh0 <vsvikramsingh305@gmail.com>
2024-08-17 00:15:12 +0530

    Completed basic design

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const input=document.getElementById("input");
+const ctof=document.getElementById("ctof");
+const ftoc=document.getElementById("ftoc");
+const submit=document.getElementById("btn");
+const result=document.getElementById("results");
+
+function temp(){
+if(ctof.Checked){
+    result.textContent="Plaese Select an radio button"
+}
+else if(ftoc.checked){
+    result.textContent="Plaese Select an radio button"
+
+}
+else{
+    result.textContent="Plaese Select an radio button"
+}
+}
